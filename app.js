e39cac727f899b6dbae710307ca21e322fdf14d8
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-12-09 12:59:51 +0100

    finish

---
+++ app.js
@@ -0,0 +1,10 @@
+
+function recursive(x) {
+    let div = document.createElement("div");
+    div.innerHTML = x;
+    document.body.append(div);
+}
+
+for (let x = 0; x < 10; x += 2){
+    recursive(x)
+}
